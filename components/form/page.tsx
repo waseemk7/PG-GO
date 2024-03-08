43cbc45972a32339ee2e870d7a43893728726968
Waseem Kurne <kurne.waseem@gmail.com>
2024-03-08 11:05:53 +0530

    picker styling

--- components/form/page.tsx
+++ components/form/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./page.module.css";
+import { FaSearch, FaBuilding, FaMapMarkerAlt } from "react-icons/fa"; // Assuming you have react-icons installed
 
 const FormComponent: React.FC = () => {
   const cities = [
@@ -16,25 +17,33 @@ const FormComponent: React.FC = () => {
   return (
     <div className={styles.formContainer}>
       <div className={styles.formRow}>
-        <label htmlFor="propertyType" className={styles.label}>
-          Property Type:
-        </label>
-        <select id="propertyType" className={styles.dropdown}>
-          <option value="PG">PG</option>
-          <option value="Hostel">Hostel</option>
-        </select>
-      </div>
-      <div className={styles.formRow}>
-        <label htmlFor="city" className={styles.label}>
-          City:
-        </label>
-        <select id="city" className={styles.dropdown}>
-          {cities.map((city) => (
-            <option key={city} value={city}>
-              {city}
+        <div className={styles.pickerWithIcon}>
+          <FaBuilding className={styles.icon} />
+          <select id="propertyType" className={styles.dropdown}>
+            <option value="" disabled hidden>
+              Select Property Type
             </option>
-          ))}
-        </select>
+            <option value="PG">PG</option>
+            <option value="Hostel">Hostel</option>
+          </select>
+        </div>
+        <div className={styles.pickerWithIcon}>
+          <FaMapMarkerAlt className={styles.icon} />
+          <select id="city" className={styles.dropdown}>
+            <option value="" disabled hidden>
+              Select City
+            </option>
+            {cities.map((city) => (
+              <option key={city} value={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button className={styles.searchButton}>
+          <FaSearch/>
+          <span>Search</span>
+        </button>
       </div>
     </div>
   );
