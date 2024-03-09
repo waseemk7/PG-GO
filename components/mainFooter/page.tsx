3a9fc1392f1c10d9d535eef62edd78a6ed69e9e1
Waseem Kurne <kurne.waseem@gmail.com>
2024-03-09 10:13:06 +0530

    footer design made responsive

--- components/mainFooter/page.tsx
+++ components/mainFooter/page.tsx
@@ -1,6 +1,5 @@
-// components/Footer.tsx
-
 import React from "react";
+import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
 import styles from "./page.module.css";
 
 const Footer: React.FC = () => {
@@ -8,19 +7,34 @@ const Footer: React.FC = () => {
     <footer className={styles.footer}>
       <div className={styles.column}>
         <h3>Contact Us</h3>
-        <p>Dummy content for Contact Us column</p>
+        <p>+91 9123456789</p>
+        <p>support@pggo.com</p>
       </div>
       <div className={styles.column}>
         <h3>About Us</h3>
-        <p>Dummy content for About Us column</p>
+        <p>Blogs</p>
+        <p>Our Story</p>
+        <p>Career</p>
+        <p>Contact Us</p>
+        <p>FAQs</p>
       </div>
       <div className={styles.column}>
         <h3>Community</h3>
-        <p>Dummy content for Community column</p>
+        <p>Terms and Conditions</p>
+        <p>Service Terms</p>
+        <p>Privacy Policy</p>
+        <p>Guest Refund Policy</p>
+        <p>Non Discrimination Policy</p>
+        <p>Booking Policy</p>
       </div>
       <div className={styles.column}>
         <h3>Social</h3>
-        <p>Dummy content for Social column</p>
+        <div className={styles.socialIcons}>
+          <FaInstagram />
+          <FaFacebook />
+          <FaTwitter />
+          <FaLinkedin />
+        </div>
       </div>
     </footer>
   );
